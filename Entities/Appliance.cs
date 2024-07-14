6548d394b414558e68c714da0755f7472ee2f6b7
PrathyushTaneti <prathyushtaneti.11@gmail.com>
2024-07-14 21:26:47 +0530

    Add project files.

---
+++ Entities/Appliance.cs
@@ -0,0 +1,18 @@
+﻿using switch_board_simulator.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_board_simulator.Entities
+{
+    public class Appliance(int id, string name, State state)
+    {
+        public int Id { get; set; } = id;
+
+        public string Name { get; set; } = name ?? throw new ArgumentNullException(nameof(name));
+
+        public State State { get; set; } = state;
+    }
+}
